8fa8531935d6cbfb61da3fcc5f04b0df82eac66c
glebalef <gleb.alef@gmail.com>
2022-09-30 15:06:28 +0300

    commit

---
+++ src/main/scripts421.sql
@@ -0,0 +1,23 @@
+ALTER TABLE student
+    ADD CONSTRAINT age_constraint CHECK (age>=16);
+
+ALTER TABLE student
+ADD CONSTRAINT name_unique UNIQUE (name);
+
+ALTER TABLE student
+ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE faculty
+ADD CONSTRAINT color_name_unique UNIQUE (color, name);
+
+ALTER TABLE student
+ALTER COLUMN age SET DEFAULT (20);
+
+
+
+
+
+
+
+
+
